dd7e16159544917281a77e0538f93de425352239
Saurabh Sharma <saurabhsharma2u@gmail.com>
2026-03-12 16:51:32 -0700

    test: fix builder integration test fixture to avoid pub warnings

--- packages/builder/test/integration/lib/main.dart
+++ packages/builder/test/integration/lib/main.dart
@@ -1,24 +1,11 @@
-// These imports are used for integration test fixtures and are not direct dependencies.
-// ignore_for_file: depend_on_referenced_packages
+// This is a test fixture for the scanner.
+// It contains strings that the scanner should detect.
 
-import 'package:flutter/material.dart';
-import 'package:iconify_sdk/iconify_sdk.dart';
+final icons = [
+  'mdi:home',
+  'lucide:rocket',
+  'tabler:star',
+];
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        IconifyIcon('mdi:home'),
-        IconifyIcon('lucide:rocket'),
-        IconifyIcon('tabler:star'),
-      ],
-    );
-  }
-}
+// The scanner also looks for the widget usage:
+// IconifyIcon('mdi:home')
